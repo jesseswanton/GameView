61350b26b26cd24954dd3a8a13ed5ddacdd1a224
jesses <jswan7692@gmail.com>
2024-11-25 22:57:40 -0700

    added auto search variable and logo

--- Develop/client/src/pages/Home.tsx
+++ Develop/client/src/pages/Home.tsx
@@ -84,8 +84,12 @@ const Home: React.FC = () => {
     }
   };
 
-  // Navigation
+  // Navigation with example query
+
   const navigate = useNavigate();
+  const handleNavigateToSearch = () => {
+    navigate("/search-video", { state: { searchQuery: "Reviews for " + "Baulder's Gate 3" } });
+  };
 
   if (error) {
     return <ErrorPage />;
@@ -103,7 +107,7 @@ const Home: React.FC = () => {
 
       <div className="main-list">
         <h2>Video game categories returned by the IGDB API:</h2>
-        <button onClick={() => navigate("/search-video")}>Go to Search Page</button>
+        <button onClick={handleNavigateToSearch}>Search for Example Query</button>
         <div>
           <Link to="/games">Click here to see the full game list!</Link>
         </div>
