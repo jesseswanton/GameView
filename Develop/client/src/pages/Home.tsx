1e0a3bff6dfbe01ab56d23e3474f518a74243ccc
Preston Stone <stonepreston05@gmail.com>
2024-11-26 22:45:59 -0700

    switched to rawg api starting styling'

--- Develop/client/src/pages/Home.tsx
+++ Develop/client/src/pages/Home.tsx
@@ -53,27 +53,20 @@ const Home: React.FC = () => {
 
   const fetchGames = async () => {
     try {
-      const response = await fetch("https://api.igdb.com/v4/games", { method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Client-ID': "ptzrhh2yuz86jp8vezagz6e025ps7v",
-          'Authorization': "Bearer ptzrhh2yuz86jp8vezagz6e025ps7v",
-        },
-        body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-    });
-
-      console.log(response)
-      
-      if (!response.ok) {
-        throw new Error("Failed to fetch games");
-      }
-
-      const data = await response.json();
-      setGameArray(data);
-    } catch (err) {
-      console.error("Failed to retrieve games:", err);
+      const response = await fetch(`https://api.rawg.io/api/games?dates=2019-10-10,2020-10-10&ordering=-added&key=${import.meta.env.VITE_RAWG_KEY}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const data = await response.json();
+    console.log(data)
+    if (data) {
+      setGameArray(data.results)
     }
+  } catch (error) {
+    console.log(error)
+  }
   };
+
 
   const handleDelete: MouseEventHandler<HTMLButtonElement> = (event) => {
     const gameId = Number(event.currentTarget.value);
