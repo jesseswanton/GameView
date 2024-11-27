2d01c72f20ef412c83f75e56e2b88549c04ffc5c
Preston Stone <stonepreston05@gmail.com>
2024-11-26 19:24:15 -0700

    troubleshooting the api

--- Develop/client/src/pages/Home.tsx
+++ Develop/client/src/pages/Home.tsx
@@ -53,19 +53,17 @@ const Home: React.FC = () => {
 
   const fetchGames = async () => {
     try {
-      const response = await fetch("https://api.igdb.com/v4/games", {
-        method: "POST",
+      const response = await fetch("https://api.igdb.com/v4/games", { method: 'POST',
         headers: {
-          "Client-ID": import.meta.env.VITE_IGDB_CLIENT_ID,
-          Authorization: `Bearer ${import.meta.env.VITE_IGDB_ACCESS_TOKEN}`,
-          "Content-Type": "application/json",
+          'Accept': 'application/json',
+          'Client-ID': "ptzrhh2yuz86jp8vezagz6e025ps7v",
+          'Authorization': "Bearer ptzrhh2yuz86jp8vezagz6e025ps7v",
         },
-        body: JSON.stringify({
-          fields: "id, name, genres.name, release_dates.date",
-          limit: 10,
-        }),
-      });
+        body: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+    });
 
+      console.log(response)
+      
       if (!response.ok) {
         throw new Error("Failed to fetch games");
       }
