c4530e03317c51b54b3919924f44c078b129400a
Joseph Stone <51794276+Petrichor4@users.noreply.github.com>
2024-12-06 09:30:55 -0700

    Update index.ts

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -13,7 +13,7 @@ const sequelize = process.env.DB_URL
       process.env.DB_USER || '',
       process.env.DB_PASSWORD,
       {
-        host: 'dpg-ct8t7k0gph6c73dhul10-a',
+        host: 'dpg-ct6gk3rv2p9s739buhjg-a',
         dialect: 'postgres',
         dialectOptions: {
           decimalNumbers: true,
